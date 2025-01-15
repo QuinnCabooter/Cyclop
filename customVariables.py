8b13f6eb7bea72520fc9ae0cfeb24e8e0d29ef3e
Quinn Cabooter <73490039+QuinnCabooter@users.noreply.github.com>
2025-01-15 15:29:48 +0100

    Edit customVariables.py

--- customVariables.py
+++ customVariables.py
@@ -11,15 +11,15 @@ In this file you will define your protocols, garment types and inner garment typ
 protocols = {
     #Standard protocol used in bodybox with the 3 exercises of 5 minutes
     "Standaard_protocol": { #Name of the protocol
-        "annotations": ["baseline", "2 min stilstaan", "eerste oefening", "stilstaan 1", "tweede oefening",
-                        "stilstaan 2", "derde oefening", "stilstaan 3", "einde"], #List of annotations
+        "annotations": ["baseline", "2_min_stilstaan", "eerste_oefening", "stilstaan_1", "tweede_oefening",
+                        "stilstaan_2", "derde_oefening", "stilstaan_3", "einde"], #List of annotations
         "times": [300, 120, 300, 60, 300, 60, 60, 60, 0] #List of times in seconds
     },
 
     #Protocol used when you want to test the functionality of the python script.
     "Supersnel_protocol": {
-        "annotations": ["baseline", "2 min stilstaan", "eerste oefening", "stilstaan 1", "tweede oefening",
-                        "stilstaan 2", "derde oefening", "stilstaan 3", "einde"],
+        "annotations": ["baseline", "2_min_stilstaan", "eerste_oefening", "stilstaan_1", "tweede_oefening",
+                        "stilstaan_2", "derde_oefening", "stilstaan_3", "einde"],
         "times": [1, 1, 1, 1, 1, 1, 1, 1, 1]
     }
 }
