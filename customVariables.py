f12132f0790e3f373bb1cc7adc2f0908a91a8954
Quinn Cabooter <73490039+QuinnCabooter@users.noreply.github.com>
2025-01-15 12:30:52 +0100

    Add comments

--- customVariables.py
+++ customVariables.py
@@ -1,22 +1,22 @@
 """
-In this file you will define your protocols. Each protocol should have a name, a list of annotations and a list of times.
+Author: Quinn Cabooter
+Date: 14-01-2025
+Last updated: 15-01-2025
 
-The code looks as follows:
-    protocols = {
-        "Protocol_name": {
-            "annotations": ["annotation1", "annotation2", "annotation3", ...],
-            "times": [Interval1, Interval2, Interval3, ...] #how long a certain segment accompanying the annotation should last.
-        }
-    }
+In this file you will define your protocols, garment types and inner garment types. Each protocol should have a name, a list of annotations and a list of times. 
+
 """
 
 #Define your protocols here
 protocols = {
-    "Standaard_protocol": {
+    #Standard protocol used in bodybox with the 3 exercises of 5 minutes
+    "Standaard_protocol": { #Name of the protocol
         "annotations": ["baseline", "2 min stilstaan", "eerste oefening", "stilstaan 1", "tweede oefening",
-                        "stilstaan 2", "derde oefening", "stilstaan 3", "einde"],
-        "times": [300, 120, 300, 60, 300, 60, 60, 60, 0]
+                        "stilstaan 2", "derde oefening", "stilstaan 3", "einde"], #List of annotations
+        "times": [300, 120, 300, 60, 300, 60, 60, 60, 0] #List of times in seconds
     },
+
+    #Protocol used when you want to test the functionality of the python script.
     "Supersnel_protocol": {
         "annotations": ["baseline", "2 min stilstaan", "eerste oefening", "stilstaan 1", "tweede oefening",
                         "stilstaan 2", "derde oefening", "stilstaan 3", "einde"],
@@ -24,6 +24,7 @@ protocols = {
     }
 }
 
+#Define your garment types here
 garments = [ "Garment1", "Garment2", "Garment3" ]
 inner_garments = [ "InnerGarment1", "InnerGarment2", "InnerGarment3" ]
 
