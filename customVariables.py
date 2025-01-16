f5b5f0053c80e5ca9c07d1c408a22c95735bb272
Quinn Cabooter <73490039+QuinnCabooter@users.noreply.github.com>
2025-01-16 14:37:49 +0100

    Set supersnel protocol as first value

--- customVariables.py
+++ customVariables.py
@@ -9,18 +9,18 @@ In this file you will define your protocols, garment types and inner garment typ
 
 #Define your protocols here
 protocols = {
-    #Standard protocol used in bodybox with the 3 exercises of 5 minutes
-    "Standaard_protocol": { #Name of the protocol
-        "annotations": ["baseline", "2_min_stilstaan", "eerste_oefening", "stilstaan_1", "tweede_oefening",
-                        "stilstaan_2", "derde_oefening", "stilstaan_3", "einde"], #List of annotations
-        "times": [300, 120, 300, 60, 300, 60, 60, 60, 0] #List of times in seconds
-    },
-
-    #Protocol used when you want to test the functionality of the python script.
-    "Supersnel_protocol": {
+        #Protocol used when you want to test the functionality of the python script.
+    "Supersnel": {
         "annotations": ["baseline", "2_min_stilstaan", "eerste_oefening", "stilstaan_1", "tweede_oefening",
                         "stilstaan_2", "derde_oefening", "stilstaan_3", "einde"],
         "times": [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    
+    #Standard protocol used in bodybox with the 3 exercises of 5 minutes
+    "Standaard": { #Name of the protocol
+        "annotations": ["baseline", "2_min_stilstaan", "eerste_oefening", "stilstaan_1", "tweede_oefening",
+                        "stilstaan_2", "derde_oefening", "stilstaan_3", "einde"], #List of annotations
+        "times": [300, 120, 300, 60, 300, 60, 60, 60, 0] #List of times in seconds
     }
 }
 
